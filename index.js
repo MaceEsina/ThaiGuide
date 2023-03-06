df2459b10146e650b9c94eb09469cb1bdbbfd791
Svetlana Esina <magistresina@gmail.com>
2023-03-06 14:52:21 +0700

    Add stripe

--- index.js
+++ index.js
@@ -14,4 +14,11 @@ document.addEventListener("DOMContentLoaded", function() {
   images.forEach((elem, inx) => {
     elem.addEventListener('click', () => onElemClick(elem, inx + 1))
   })
+
+  document.getElementById('thai-guide-buy').onclick = function() {
+    const url = 'https://buy.stripe.com/3cs8zkfY3ekDefS3cc'
+    // const url = 'https://buy.stripe.com/28o3f04fla4n5Jm3cd'
+    window.open(url)
+    return false
+  };
 });
