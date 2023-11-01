64a17bbf9249ac2926ee082c27b27d7886d0315a
Svetlana Esina <magistresina@gmail.com>
2023-11-01 07:20:25 +0700

    Add bangkok guide, refactor thanks page

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   const images = document.querySelectorAll('#image-row > div')
   const image = document.getElementById('image1')
+  const downloadBtn = document.getElementById('download-btn')
+  const thailandBuy = document.getElementById('thai-guide-buy')
+  const bangkokBuy = document.getElementById('bangkok-guide-buy')
 
   const onElemClick = (elem, inx) => {
     images.forEach(item => {
@@ -15,9 +18,35 @@ document.addEventListener("DOMContentLoaded", function() {
     elem.addEventListener('click', () => onElemClick(elem, inx + 1))
   })
 
-  document.getElementById('thai-guide-buy').onclick = function() {
-    const url = 'https://buy.stripe.com/7sI00U48k9FF5XyaEG'
-    window.open(url, '_self')
-    return false
-  };
+  if (thailandBuy) {
+    thailandBuy.onclick = function() {
+      const url = 'https://buy.stripe.com/7sI00U48k9FF5XyaEG'
+      window.open(url, '_self')
+      return false
+    };
+  }
+
+  if (bangkokBuy) {
+    bangkokBuy.onclick = function() {
+      // TODO change url 
+      const url = 'https://buy.stripe.com/7sI00U48k9FF5XyaEG'
+      window.open(url, '_self')
+      return false
+    };
+  }
+
+  if (downloadBtn) {
+    downloadBtn.onclick = function() {
+      const urlParams = new URLSearchParams(window.location.search)
+      const fileName = urlParams.get('file_name')
+      const link = document.createElement('a')
+
+      link.setAttribute('download', fileName)
+      link.href = `./assets/${fileName}.pdf`
+      link.style.display = 'none'
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    };
+  }
 });
